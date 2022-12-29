c8a920457b31d7794d952f99cc179c0222d43037
Михаил <119431623+Mihail-Lebedev@users.noreply.github.com>
2022-12-29 12:04:32 +0400

    Update test_users_bot.py

--- test_users_bot.py
+++ test_users_bot.py
@@ -6,7 +6,7 @@ from faker import Faker
 from telebot import TeleBot, types
 
 # TODO: вставить свой токен
-TOKEN = '5401620391:AAHApI2qyefNUwqOl24FbM1oF6P6YYMJapY'
+TOKEN = 'вставьте свой токен'
 bot = TeleBot(TOKEN, parse_mode='html')
 # утилита для генерации номеров кредитных карт
 # указываем язык - русский
